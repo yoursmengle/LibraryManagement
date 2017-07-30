fdf2d7484edf904fb7a59cba2a020e109ba5732d
yoursmengle <yoursmengle@126.com>
2017-07-30 16:59:48 +0800

    add some class

--- management/models.py
+++ management/models.py
@@ -8,10 +8,36 @@ class MyUser(models.Model):
     user = models.OneToOneField(User)
     nickname = models.CharField(max_length=16)
     permission = models.IntegerField(default=1)
+    mobile = models.CharField(max_length=16)
+    weichat = models.CharField(max_length=128)
+    alipay = models.CharField(max_lengthy=128)
 
     def __unicode__(self):
         return self.user.username
 
+class Question(models.Model):
+    user = models.ForeignKey(MyUser)
+    title = models.CharField(max_length=128)
+    content = models.TextField(max_length=4096)
+    image = models.ImageField()
+    start_time = models.DateField()
+    end_time = models.DateField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    user = models.ForeignKey(MyUser)
+    content = models.TextField(max_length=4096)
+    image = models.ImageField()
+    time = models.DateField()
+    place = models.IntegerField()
+
+class Compare(models.Model):
+    user = models.ForeignKey(MyUser)
+    answerA = models.ForeignKey(Answer)
+    answerB = models.ForeignKey(Answer)
+    AisBetter = models.BoolField()
+    BisBetter = models.BoolField()
+    DontSure = models.BoolField()
 
 class Book(models.Model):
     name = models.CharField(max_length=128)
